a9f726292ca8e2b42e38743720eab6f6877fedf5
sszabo@iastate.edu <sszabo@iastate.edu>
2023-09-08 16:24:32 -0500

    max pooling layer work

--- lab2/src/layers/MaxPooling.cpp
+++ lab2/src/layers/MaxPooling.cpp
@@ -7,20 +7,64 @@
 #include "Layer.h"
 
 namespace ML {
+const fp32 MaxPoolingLayer::compute3DIntermediateResult(const LayerData& ifMap, const size curRow,
+                                                        const size curCol,
+                                                        const size curChan) const {
+    fp32 max = ifMap.getData<Array3D_fp32>()[curRow][curCol][curChan];
+    for (size rowIdx = curRow; rowIdx < (curRow + POOL_SIZE); rowIdx++) {
+        for (size colIdx = curCol; colIdx < (curCol + POOL_SIZE); colIdx++) {
+            if (ifMap.getData<Array3D_fp32>()[rowIdx][colIdx][curChan] > max) {
+                max = ifMap.getData<Array3D_fp32>()[rowIdx][colIdx][curChan];
+            }
+        }
+    }
+    return max;
+}
+
 void MaxPoolingLayer::computeNaive(const LayerData& dataIn) const {
-    // TODO 
+    if (!dataIn.isAlloced() || !dataIn.isValid()) {
+        logError("ERROR: dataIn not allocated or not valid");
+        exit(1);
+    }
+    if (!this->isOutputBufferAlloced() || !this->getOutputData().isValid()) {
+        logError("ERROR: output buffer not allocated or not valid");
+        exit(1);
+    }
+    const auto maxRowOut = this->getOutputParams().dims.at(0);
+    const auto maxColOut = this->getOutputParams().dims.at(1);
+    const auto maxChan = dataIn.getParams().dims.at(2);
+
+    // error handling
+    if (maxRowOut != dataIn.getParams().dims.at(0) / POOL_STRIDE) {
+        logError("ERROR: maxRowOut != dataIn.getParams().dims.at(0) / POOL_STRIDE");
+        exit(1);
+    }
+    if (maxColOut != dataIn.getParams().dims.at(1) / POOL_STRIDE) {
+        logError("ERROR: maxColOut != dataIn.getParams().dims.at(1) / POOL_STRIDE");
+        exit(1);
+    }
+
+    for (size chanIdx = 0; chanIdx < maxChan; chanIdx++) {
+        for (size rowIdx = 0; rowIdx < maxRowOut; rowIdx++) {
+            for (size colIdx = 0; colIdx < maxColOut; colIdx++) {
+                const auto& outData = this->getOutputData().getData<Array3D_fp32>();
+                outData[rowIdx][colIdx][chanIdx] = compute3DIntermediateResult(
+                    dataIn, rowIdx * POOL_STRIDE, colIdx * POOL_STRIDE, chanIdx);
+            }
+        }
+    }
 }
 
 void MaxPoolingLayer::computeThreaded(const LayerData& dataIn) const {
-    // TODO 
+    // TODO
 }
 
 void MaxPoolingLayer::computeTiled(const LayerData& dataIn) const {
-    // TODO 
+    // TODO
 }
 
 void MaxPoolingLayer::computeSIMD(const LayerData& dataIn) const {
-    // TODO 
+    // TODO
 }
 
 }  // namespace ML
