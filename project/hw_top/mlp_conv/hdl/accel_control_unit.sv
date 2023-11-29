9792440145c3d622b48781a41eca2f5c87dfb5c3
SomaSz <soma.szabo15@gmail.com>
2023-11-29 00:45:54 -0600

    reformatting and control unit WIP

--- project/hw_top/mlp_conv/hdl/accel_control_unit.sv
+++ project/hw_top/mlp_conv/hdl/accel_control_unit.sv
@@ -2,58 +2,137 @@
 
 `timescale 1ns/1ps
 module accel_control_unit #(
-    parameter AXI_S_DW = 32,
-    parameter AXI_M_DW = 32,
+    parameter C_S00_AXI_DATA_WIDTH = 32,
+    parameter C_M00_AXI_DATA_WIDTH = 32,
     parameter PE_ROWS = 5,
-    parameter PE_COLS = 5
+    parameter PE_COLS = 5,
+    parameter MAC_PIPE_DEPTH = 2
   )(
     input CLK,
     input RESETN,
 
     // accelerator interface
     // inputs
-    input [0:PE_COLS-1] mac_done [0:PE_ROWS-1],             // true every time a MAC operation is done and ready to pass forward
-    input [0:PE_COLS-1] status [0:PE_ROWS-1],               // indicates if the output register is valid
+    input [C_S00_AXI_DATA_WIDTH-1:0] params_reg,
+    input [C_S00_AXI_DATA_WIDTH-1:0] weight_base_addr,
+    input [C_S00_AXI_DATA_WIDTH-1:0] input_base_addr,
+    input [C_S00_AXI_DATA_WIDTH-1:0] output_base_addr,
+    input [C_S00_AXI_DATA_WIDTH-1:0] mem_ctrl,
+    input [0:PE_COLS-1] MAC_DONE [0:PE_ROWS-1],             // true every time a MAC operation is done and ready to pass forward
 
     // outputs
-    output [0:PE_COLS-1] weight_reg_wr_ctrl [0:PE_ROWS-1],
+    output [2:0] weight_row_wr_ctrl,                        // index of weight row to write to
     output [0:PE_COLS-1] add_mux_ctrl [0:PE_ROWS-1],
     output [0:PE_COLS-1] stall_ctrl [0:PE_ROWS-1],
     output [0:PE_COLS-1] resetn_mac_ctrl [0:PE_ROWS-1],
     output [3:0] row_out_mux_ctrl [0:PE_ROWS-1],            // 9:1 mux for each row of MACs
-    output [2:0] psum_out_ctrl,                             // 5:1 mux controlling which row goes to output storage or feedback
-    output psum_feedback_ctrl,                              // 2:1 mux controls which psum (from array or from output storage) proceeds to output storage (if out_storage_wr_en) or feeds back to 1st MAC unit
-    output out_storage_wr_en                               // output storage register write enable
+    output [2:0] psum_out_mux_ctrl,                         // 5:1 mux controlling which row goes to output storage or feedback
+    output out_storage_wr_en                                // output storage register write enable
 
     // AXI interfaces?
 
   );
 
-  wire [AXI_M_DW-1:0] debug;
+  wire [C_M00_AXI_DATA_WIDTH-1:0] debug;
+
+
+  // parameter wires
+  wire [3:0] param_R;   // filter height
+  wire [3:0] param_S;   // filter width
+  wire [3:0] param_U;
+  wire [3:0] param_tile_size;
+  wire [11:0] param_C;
+  wire param_valid;
+  wire param_reset;
+
+  assign param_R = params_reg[3:0];
+  assign param_S = params_reg[7:4];
+  assign param_U = params_reg[11:8];
+  assign param_tile_size = params_reg[15:12];
+  assign param_C = params_reg[27:16];
+  assign param_valid = params_reg[30];
+  assign param_reset = params_reg[31];
+
+  // add mux control logic
+  always_ff @(posedge CLK) begin
+    if (RESETN == 1'b0) begin
+      add_mux_ctrl <= 0;
+    end
+    else begin
+      // first S columns will be set to 1, rest to 0
+      // rows greater than R will be set to 0
+      if (param_valid == 1'b1) begin
+        for (int r = 0; r < PE_ROWS; r++) begin
+          for (int c = 0; c < PE_COLS; c++) begin
+            if (c < unsigned'(param_S)) begin
+              if (r < unsigned'(param_R)) begin
+                add_mux_ctrl[r][c] <= 1'b1;
+              end
+              else begin
+                add_mux_ctrl[r][c] <= 1'b0;
+              end
+            end
+            else begin
+              add_mux_ctrl[r][c] <= 1'b0;
+            end
+          end
+        end
+      end
+    end
+  end
+
+  // row out mux control logic
+  always_ff @(posedge CLK) begin
+    if (RESETN == 1'b0) begin
+      row_out_mux_ctrl <= 0;
+    end
+    else begin
+      // each row will get the same 4-bit value
+      // set to the value of tile size - 1
+      if (param_valid == 1'b1) begin
+        for (int r = 0; r < PE_ROWS; r++) begin
+          row_out_mux_ctrl[r] <= param_tile_size - 1;
+        end
+      end
+    end
+  end
+
+  // psum out mux control logic
+  always_ff @(posedge CLK) begin
+    if (RESETN == 1'b0) begin
+      psum_out_mux_ctrl <= 0;
+    end
+    else begin
+      // set
+      if (param_valid == 1'b1) begin
+        psum_out_mux_ctrl <= param_R - 1;
+      end
+    end
+  end
 
 
 
   // states for saving parameters
   typedef enum logic {
-    IDLE_SP,
-    SAVE_PARAMS
-  } state_type_save_params;
+            IDLE_SP,
+            SAVE_PARAMS
+          } state_type_save_params;
   state_type_save_params st_save_params, st_save_params_next;
 
 
   // states for buffering weights
   typedef enum logic {
-    IDLE_BW,
-    BUFFER_WEIGHTS
-  } state_type_buff_w;
+            IDLE_BW,
+            BUFFER_WEIGHTS
+          } state_type_buff_w;
   state_type_buff_w st_buff_w, st_buff_w_next;
 
 
   // states for buffering inputs
   typedef enum logic {
-    IDLE_BI,
-    BUFFER_INPUTS
-  } state_type_buff_i;
+            IDLE_BI,
+            BUFFER_INPUTS
+          } state_type_buff_i;
   state_type_buff_i st_buff_i, st_buff_i_next;
 
 
@@ -63,10 +142,10 @@ module accel_control_unit #(
 
   // states for writing outputs
   typedef enum logic [1:0] {
-    IDLE_BO,
-    BUFFER_OUTPUTS,
-    WRITE_OUTPUTS
-  } state_type_wr_o;
+            IDLE_BO,
+            BUFFER_OUTPUTS,
+            WRITE_OUTPUTS
+          } state_type_wr_o;
   state_type_wr_o st_wr_o, st_wr_o_next;
 
 
--- project/hw_top/mlp_conv/hdl/mlp_conv_v1_0_PE.vhd
+++ project/hw_top/mlp_conv/hdl/mlp_conv_v1_0_PE.vhd
@@ -17,8 +17,6 @@
 -- Additional Comments:
 -- 
 ----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
@@ -33,74 +31,74 @@ use IEEE.numeric_std.all;
 --use UNISIM.VComponents.all;
 
 entity mlp_conv_v1_0_PE is
-    generic(
-        INPUT_WIDTH : integer := 8;
-        OUTPUT_WIDTH: integer := 32
-    );
-    port (
-        ACLK	     : in	std_logic;
-		ARESETN	     : in	std_logic; 
-		-- Trimmed down AXIS slave data interface for input and weight
-		input	     : in	std_logic_vector(INPUT_WIDTH-1 downto 0); 
-		weight       : in	std_logic_vector(INPUT_WIDTH-1 downto 0);
-		add_mux_ctrl : in   std_logic;
-		input_valid	 : in	std_logic;
-		-- Stalls pipeline (WE DO NOT EVER PROGRESS OUTPUT WHEN STALLED)
-		stall_ctl    : in   std_logic;
-        
-        -- Other MAC's output
-		add_val      : in std_logic_vector(OUTPUT_WIDTH-1 downto 0);
-		
-        -- AXIS master accumulate result out interface
-		output 	: out	std_logic_vector(OUTPUT_WIDTH-1 downto 0)
-    );
+  generic (
+    INPUT_WIDTH  : integer := 8;
+    OUTPUT_WIDTH : integer := 32
+  );
+  port (
+    ACLK    : in std_logic;
+    ARESETN : in std_logic;
+    -- Trimmed down AXIS slave data interface for input and weight
+    input        : in std_logic_vector(INPUT_WIDTH - 1 downto 0);
+    weight       : in std_logic_vector(INPUT_WIDTH - 1 downto 0);
+    add_mux_ctrl : in std_logic;
+    input_valid  : in std_logic;
+    -- Stalls pipeline (WE DO NOT EVER PROGRESS OUTPUT WHEN STALLED)
+    stall_ctl : in std_logic;
+
+    -- Other MAC's output
+    add_val : in std_logic_vector(OUTPUT_WIDTH - 1 downto 0);
+
+    -- AXIS master accumulate result out interface
+    output : out std_logic_vector(OUTPUT_WIDTH - 1 downto 0)
+  );
 end mlp_conv_v1_0_PE;
 
 architecture arch_imp of mlp_conv_v1_0_PE is
-    -- Internal Signals
-	signal product    : unsigned(INPUT_WIDTH*2-1 downto 0);    
-	signal stalled    : std_logic;
-	signal output_reg : std_logic_vector(OUTPUT_WIDTH-1 downto 0);
-	-- Mac stages
-    type PIPE_STAGES is (MULT_INPUT, ADD, SET_OUTPUT);
-	-- Debug signals, make sure we aren't going crazy
-    signal mac_debug : std_logic_vector(31 downto 0);
+  -- Internal Signals
+  signal product    : unsigned(INPUT_WIDTH * 2 - 1 downto 0);
+  signal stalled    : std_logic;
+  signal output_reg : std_logic_vector(OUTPUT_WIDTH - 1 downto 0);
+  -- Mac stages
+  type PIPE_STAGES is (MULT_INPUT, ADD, SET_OUTPUT);
+  -- Debug signals, make sure we aren't going crazy
+  signal mac_debug : std_logic_vector(31 downto 0);
 
 begin
-    stalled <= (not input_valid) or stall_ctl;
-	-- Debug Signals
-    mac_debug <= x"00000001";  -- Double checking sanity
-   
-   process (ACLK) is
-   begin 
-    if rising_edge(ACLK) then  -- Rising Edge
+  stalled <= (not input_valid) or stall_ctl;
+  -- Debug Signals
+  mac_debug <= x"00000001"; -- Double checking sanity
+
+  process (ACLK) is
+  begin
+    if rising_edge(ACLK) then -- Rising Edge
       -- Reset values if reset is low
-      if ARESETN = '0' then  -- Reset
-		product <= (others => '0');
-		output <= (others => '0');
+      if ARESETN = '0' then -- Reset
+        product <= (others => '0');
+        output  <= (others => '0');
       else
         for i in PIPE_STAGES'left to PIPE_STAGES'right loop
-            case i is  -- Stages
-                when MULT_INPUT =>
-                    if stalled = '1' then
-                        product <= unsigned(input) * unsigned(weight);
-                    end if;
-				when ADD =>
-				    if stalled = '1' then
-                if add_mux_ctrl = '0' then
-				          output_reg <= std_logic_vector(unsigned(add_val) + product);
-                else 
+          case i is -- Stages
+            when MULT_INPUT =>
+              if stalled = '0' then
+                product <= unsigned(input) * unsigned(weight);
+              end if;
+            when ADD =>
+              if stalled = '0' then
+                if add_mux_ctrl = '1' then
+                  output_reg <= std_logic_vector(unsigned(add_val) + product);
+                else
                   output_reg <= add_val;
                 end if;
-				    end if;
-				when SET_OUTPUT =>
-				    if stalled = '1' then
-				        output <= output_reg;
-				    end if;
-            end case;  -- Stages
-		end loop;  -- Stages
-      end if;  -- Reset
-    end if;  -- Rising Edge
-   end process;
+              end if;
+            when SET_OUTPUT =>
+              if stalled = '0' then
+                output <= output_reg;
+              end if;
+          end case; -- Stages
+        end loop; -- Stages
+      end if; -- Reset
+    end if; -- Rising Edge
+  end process;
 
 end arch_imp;
