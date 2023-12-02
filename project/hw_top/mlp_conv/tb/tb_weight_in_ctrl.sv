d6dea2c9faa26b970d4ad11f4f7f3d26dc89ecec
sszabo@iastate.edu <sszabo@iastate.edu>
2023-12-02 02:32:46 -0600

    weight in ctrl and TB done

--- project/hw_top/mlp_conv/tb/tb_weight_in_ctrl.sv
+++ project/hw_top/mlp_conv/tb/tb_weight_in_ctrl.sv
@@ -9,13 +9,16 @@ module tb_weight_in_ctrl();
   localparam WS_IN_DIFF = WS_WIDTH - INPUT_WIDTH;
   localparam FIFO_DEPTH = 16;
 
-  const int MAX_FIFO_CLEARS = 100;
+  const int MAX_FIFO_CLEARS = 1000;
+  int num_fifo_clears;
 
   // verification variables
   logic [WS_WIDTH-1:0] scb_ws [0:WS_DEPTH-1];
   int scb_ws_idx;
   int wr_count;
+  logic ws_full_pulse_captured;
 
+  int temp_count;
   int clear_fifo_cycles;
   int load_ws_cycles;
   int extra_cycles;
@@ -77,6 +80,7 @@ module tb_weight_in_ctrl();
     end
     scb_ws_idx = 0;
     wr_count = 0;
+    ws_full_pulse_captured = 0;
     clear_fifo_cycles = 1;
     load_ws_cycles = 1;
     extra_cycles = 1;
@@ -84,6 +88,7 @@ module tb_weight_in_ctrl();
 
   task automatic reset_all();
     reset_tb();
+    num_fifo_clears = 0;
     // reset DUT
     resetn = 0;
     clear_fifo = 0;
@@ -111,7 +116,8 @@ module tb_weight_in_ctrl();
       #1;
       fifo_wr_cmd = $random;
       if (fifo_wr_cmd == 1) begin
-        fifo_wr_data = scb_ws[scb_ws_idx];
+        // * Justified to MSB!!
+        fifo_wr_data = scb_ws[scb_ws_idx][WS_WIDTH-1:WS_IN_DIFF];
         scb_ws_idx++;
       end
       if (fifo_full == 1) begin
@@ -120,36 +126,45 @@ module tb_weight_in_ctrl();
       end
       @(posedge clk);
     end
+    fifo_wr_cmd = 0;
     scb_ws_idx = 0;
   endtask
 
   task automatic do_load_ws();
-    #1;
-    load_ws = 1;
     load_ws_cycles = $urandom_range(1, 8);
     while (load_ws_cycles > 0) begin
+      #1;
+      load_ws = 1;
       load_ws_cycles--;
       @(posedge clk);
+      if (ws_full == 1)
+        ws_full_pulse_captured = 1;
     end
+    #1;
     load_ws = 0;
   endtask
 
   task automatic do_clear_fifo();
-    #1;
-    clear_fifo = 1;
-    clear_fifo_cycles = $urandom_range(1, 8);
+    temp_count = $urandom_range(1, 8);
+    clear_fifo_cycles = temp_count;
     while (clear_fifo_cycles > 0) begin
+      #1;
+      clear_fifo = 1;
       clear_fifo_cycles--;
       @(posedge clk);
     end
+    #1;
     clear_fifo = 0;
+    // wait for cycle after reset befor FIFO can accept data
+    if (temp_count == 1)
+      @(posedge clk);
   endtask
 
   // checks the final weight store output after all the data has been sent (to FIFO) and loaded in (to WS)
-  task automatic check_ws_output();
+  task automatic wait_ws_full();
     // ensure ws is full (assuming do_load_ws() was called)
     int limit = 20;
-    while (ws_full == 0 && limit > 0) begin
+    while (ws_full == 0 && ws_full_pulse_captured == 0 && limit > 0) begin
       #1;
       @(posedge clk);
       limit--;
@@ -158,15 +173,47 @@ module tb_weight_in_ctrl();
       $display("ERROR: ws_full = %0d, should be 1", ws_full);
       $finish;
     end
+  endtask
 
+  function automatic void check_ws_output();
     // check if the data is correct
-    for (int i = 0; i < param_r; i++) begin
-      if (ws_rd_data[i] != scb_ws[i]) begin
-        $display("ERROR: ws_rd_data[%0d] = %0d, scb_ws[%0d] = %0d", i, ws_rd_data[i], i, scb_ws[i]);
-        $finish;
+    if (param_s == 4) begin
+      for (int i = 0; i < param_r; i++) begin
+        if (ws_rd_data[i][WS_WIDTH-1:WS_IN_DIFF] != scb_ws[i][WS_WIDTH-1:WS_IN_DIFF]) begin
+          $display("ERROR: ws_rd_data[%0d] = %0d, scb_ws[%0d] = %0d", i, ws_rd_data[i][WS_WIDTH-1:WS_IN_DIFF], i, scb_ws[i][WS_WIDTH-1:WS_IN_DIFF]);
+          $finish;
+        end
       end
     end
-  endtask
+    else if (param_s == 3) begin
+      for (int i = 0; i < param_r; i++) begin
+        if (ws_rd_data[i][WS_WIDTH-1:2*WS_IN_DIFF] != scb_ws[i][WS_WIDTH-1:2*WS_IN_DIFF]) begin
+          $display("ERROR: ws_rd_data[%0d] = %0d, scb_ws[%0d] = %0d", i, ws_rd_data[i][WS_WIDTH-1:2*WS_IN_DIFF], i, scb_ws[i][WS_WIDTH-1:2*WS_IN_DIFF]);
+          $finish;
+        end
+      end
+    end
+    else if (param_s == 2) begin
+      for (int i = 0; i < param_r; i++) begin
+        if (ws_rd_data[i][WS_WIDTH-1:3*WS_IN_DIFF] != scb_ws[i][WS_WIDTH-1:3*WS_IN_DIFF]) begin
+          $display("ERROR: ws_rd_data[%0d] = %0d, scb_ws[%0d] = %0d", i, ws_rd_data[i][WS_WIDTH-1:3*WS_IN_DIFF], i, scb_ws[i][WS_WIDTH-1:3*WS_IN_DIFF]);
+          $finish;
+        end
+      end
+    end
+    else if (param_s == 1) begin
+      for (int i = 0; i < param_r; i++) begin
+        if (ws_rd_data[i][WS_WIDTH-1:4*WS_IN_DIFF] != scb_ws[i][WS_WIDTH-1:4*WS_IN_DIFF]) begin
+          $display("ERROR: ws_rd_data[%0d] = %0d, scb_ws[%0d] = %0d", i, ws_rd_data[i][WS_WIDTH-1:4*WS_IN_DIFF], i, scb_ws[i][WS_WIDTH-1:4*WS_IN_DIFF]);
+          $finish;
+        end
+      end
+    end
+    else begin
+      $display("ERROR: invalid param_s = %0d", param_s);
+      $finish;
+    end
+  endfunction
 
   task automatic test_4x4_or_less();
     $display("test_4x4_or_less");
@@ -175,20 +222,21 @@ module tb_weight_in_ctrl();
     set_rand_rs();
     send_all_rand_data_to_fifo();
     do_load_ws();
+    wait_ws_full();
 
     check_ws_output();
     $display("test_4x4_or_less PASSED");
   endtask
 
   task automatic test_rand_4x4_or_less();
-    $display("test_5x5_or_more");
+    $display("test_rand_4x4_or_less");
     reset_all();
 
-    int num_fifo_clears = 0;
     while (num_fifo_clears < MAX_FIFO_CLEARS) begin
       set_rand_rs();
       send_all_rand_data_to_fifo();
       do_load_ws();
+      wait_ws_full();
       check_ws_output();
 
       // reset tb variables and clear FIFO
@@ -197,14 +245,15 @@ module tb_weight_in_ctrl();
       num_fifo_clears++;
     end
 
-    $display("test_5x5_or_more PASSED");
+    $display("test_rand_4x4_or_less PASSED");
   endtask
 
   // simulation
   initial begin
     $display("tb_weight_in_ctrl");
 
-    test_4x4_or_less();
+    // test_4x4_or_less();
+    test_rand_4x4_or_less();
     $display("\n");
 
     $display("tb_weight_in_ctrl PASSED");
